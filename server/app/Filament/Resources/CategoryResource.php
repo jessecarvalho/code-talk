f235d549fe5f862607eaa061f5d1e12a9ba40bb2
jessecarvalho <jesseelias80@gmail.com>
2024-01-25 10:50:39 -0300

    [feat] Created resources for Categories

---
+++ server/app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label('Name')
+                    ->rules(['required', 'string', 'max:255']),
+                Forms\Components\TextInput::make('slug')
+                    ->label('Slug')
+                    ->rules(['required', 'string', 'max:255']),
+                Forms\Components\RichEditor::make('description')
+                    ->label('Description')
+                    ->rules(['required', 'string'])
+                    ->columnSpan("full"),
+                Forms\Components\FileUpload::make('banner')
+                    ->label('Banner')
+                    ->image()
+                    ->imageEditor()
+                    ->directory('/img/panel')
+                    ->downloadable()
+                    ->previewable(true),
+                Forms\Components\FileUpload::make('icon')
+                    ->label('Icon')
+                    ->image()
+                    ->imageEditor()
+                    ->directory('/img/panel')
+                    ->downloadable()
+                    ->previewable(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->label('Slug')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
