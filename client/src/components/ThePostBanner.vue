c73e0f51cc1430223298cebbd4fc0468727e4a75
jessecarvalho <jesseelias80@gmail.com>
2024-01-27 14:44:46 -0300

    [feat] Added treatment to show banner image sent from server

--- client/src/components/ThePostBanner.vue
+++ client/src/components/ThePostBanner.vue
@@ -1,4 +1,35 @@
 <template>
-    <section class="baner" id="banner">
-    </section>
+  <section id="post-banner" v-if="post" :style="{backgroundImage: `url('http://localhost:8000/storage/' + post.banner)`}">
+    <div id="banner-text">
+    </div>
+  </section>
 </template>
+<script>
+import formatDate from '../helpers/FormatDate'
+export default {
+  name: 'ThePost',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  watch: {
+    post() {
+      if (this.post) {
+        this.updateImage();
+      }
+    }
+  },
+  methods: {
+    formatDate,
+    updateImage() {
+      console.log(this.post.banner)
+      this.$nextTick(() => {
+        const banner = document.getElementById('post-banner');
+        banner.style.backgroundImage = `url('http://localhost:8000/storage/${this.post.banner}')`;
+      })
+    }
+  }
+}
+</script>
