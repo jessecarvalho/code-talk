fce57c8f3e08b2f36ebc56d6eac10cbfb4f800d2
jessecarvalho <jesseelias80@gmail.com>
2024-01-27 14:45:17 -0300

    [feat] Added treatment to show post info received from server

--- client/src/components/ThePost.vue
+++ client/src/components/ThePost.vue
@@ -1,19 +1,21 @@
 <template>
     <div id="post-grid">
         <div id="post">
-            <h2>Look at this beautiful cat</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laborum reprehenderit illo ex, fugit ut eius delectus eum hic inventore pariatur. Cupiditate autem praesentium maiores amet perferendis numquam rem id?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laborum reprehenderit illo ex, fugit ut eius delectus eum hic inventore pariatur. Cupiditate autem praesentium maiores amet perferendis numquam rem id?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laborum reprehenderit illo ex, fugit ut eius delectus eum hic inventore pariatur. Cupiditate autem praesentium maiores amet perferendis numquam rem id?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laborum reprehenderit illo ex, fugit ut eius delectus eum hic inventore pariatur. Cupiditate autem praesentium maiores amet perferendis numquam rem id?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laborum reprehenderit illo ex, fugit ut eius delectus eum hic inventore pariatur. Cupiditate autem praesentium maiores amet perferendis numquam rem id?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laborum reprehenderit illo ex, fugit ut eius delectus eum hic inventore pariatur. Cupiditate autem praesentium maiores amet perferendis numquam rem id?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laborum reprehenderit illo ex, fugit ut eius delectus eum hic inventore pariatur. Cupiditate autem praesentium maiores amet perferendis numquam rem id?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laborum reprehenderit illo ex, fugit ut eius delectus eum hic inventore pariatur. Cupiditate autem praesentium maiores amet perferendis numquam rem id?</p>
+            <h1>{{post.title}}</h1>
+
+            <div v-for="item in post.content" :key="item.id">
+              <component :is="'h' + item.data.level" v-if="item.type === 'heading'" v-html="item.data.content"></component>
+              <p v-else-if="item.type === 'paragraph'" v-html="item.data.content"></p>
+              <img v-else-if="item.type === 'image'" :src="item.data.content" alt="Imagem do post">
+              <div v-else v-html="item.data.content"></div>
+            </div>
+
             <div id="excerpt">
                 <hr>
-                <p id="author"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg> Fulano de tal</p>
-                <p id="date"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M96 32V64H48C21.5 64 0 85.5 0 112v48H448V112c0-26.5-21.5-48-48-48H352V32c0-17.7-14.3-32-32-32s-32 14.3-32 32V64H160V32c0-17.7-14.3-32-32-32S96 14.3 96 32zM448 192H0V464c0 26.5 21.5 48 48 48H400c26.5 0 48-21.5 48-48V192z"/></svg> 23/03/2000</p>
+                <p id="author"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
+                  {{ post.user_id }}</p>
+                <p id="date"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M96 32V64H48C21.5 64 0 85.5 0 112v48H448V112c0-26.5-21.5-48-48-48H352V32c0-17.7-14.3-32-32-32s-32 14.3-32 32V64H160V32c0-17.7-14.3-32-32-32S96 14.3 96 32zM448 192H0V464c0 26.5 21.5 48 48 48H400c26.5 0 48-21.5 48-48V192z"/></svg>
+                  {{ formatDate(post.created_at) }}</p>
             </div>
         </div>
         <div class="related">
@@ -46,5 +48,17 @@
 </template>
 
 <script>
-
+import formatDate from '../helpers/FormatDate'
+export default {
+  name: 'ThePost',
+  methods: {
+    formatDate,
+  },
+  props: {
+    post: {
+      type: Array,
+      required: true
+    }
+  },
+}
 </script>
