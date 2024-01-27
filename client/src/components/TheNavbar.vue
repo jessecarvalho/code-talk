2c0a70d5e8297eaa291253527cd6735bf95fe62e
jessecarvalho <jesseelias80@gmail.com>
2024-01-27 14:57:48 -0300

    [Fix] Navbar menu names and urls

--- client/src/components/TheNavbar.vue
+++ client/src/components/TheNavbar.vue
@@ -4,9 +4,9 @@
             <img src="../assets/imgs/logo.png" alt="Logo da BloGato">
         </a>
         <ul class="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Contato</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
         </ul>
     </header>
 </template>
