ea10845f5f3d310f08605954b337b2aab7559db6
jessecarvalho <jesseelias80@gmail.com>
2024-01-27 14:46:40 -0300

    [feat] Created fetch to receive post info from server

--- client/src/views/PostView.vue
+++ client/src/views/PostView.vue
@@ -1,8 +1,8 @@
 <template>
     <main>
       <the-navbar />
-      <the-post-banner />
-      <the-post />
+      <the-post-banner :post="post" />
+      <the-post :post="post" />
       <the-footer />
     </main>
 </template>
@@ -12,4 +12,27 @@
     import ThePostBanner from '../components/ThePostBanner.vue'
     import ThePost from '../components/ThePost.vue'
     import TheFooter from '../components/TheFooter.vue'
+</script>
+
+<script>
+import ApiService from '../services/ApiService';
+import { useRoute } from 'vue-router';
+
+export default {
+  data() {
+    return {
+      post: []
+    };
+  },
+  created() {
+    const route = useRoute();
+    this.fetchData('/post/' + route.params.id);
+  },
+  methods: {
+    async fetchData(url) {
+      this.post = await ApiService.fetchData(url);
+      console.log("carregou")
+    }
+  }
+};
 </script>
