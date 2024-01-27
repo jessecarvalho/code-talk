73dbf035b0a4920ee262c765bb4ad4e33ac5260a
jessecarvalho <jesseelias80@gmail.com>
2024-01-27 15:47:13 -0300

    [fix] Changed contact page

--- client/src/components/TheContactBanner.vue
+++ client/src/components/TheContactBanner.vue
@@ -1,4 +1,10 @@
 <template>
     <section class="banner" id="contact-banner">
+      <div class="content">
+        <img src="/src/assets/imgs/coffee.gif">
+        <h2>So let's have a virtual coffee</h2>
+      </div>
     </section>
 </template>
+<script setup>
+</script>
