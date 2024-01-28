1bce697737386e1395210b43b513c8b1adb344c6
jessecarvalho <jesseelias80@gmail.com>
2024-01-28 11:26:05 -0300

    [feat] Added button in new component

---
+++ client/src/components/TheButtonSeeAllPosts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full d-flex">
+    <a href="/posts">
+      <button class="btn btn-purple">
+        Veja Mais Posts
+      </button>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+
+  div {
+    justify-content: center;
+  }
+
+</style>
+
+<script setup>
+
+</script>
