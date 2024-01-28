fcdea27bdda9f222613609565ffc42e5ee3a4737
jessecarvalho <jesseelias80@gmail.com>
2024-01-28 11:28:03 -0300

    [feat] Created banner component to the all posts page

---
+++ client/src/components/ThePostsBanner.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="banner" id="posts-banner">
+    <div class="content">
+      <img src="/src/assets/imgs/use-computer-job.gif" alt="Coffee Gif">
+      <h2>Todos os posts</h2>
+    </div>
+  </section>
+</template>
+<script setup>
+</script>
