1cf8de3de8b35b002dff72306ed09b2b65eaff7b
jessecarvalho <jesseelias80@gmail.com>
2024-01-28 11:25:20 -0300

    [feat] View to show all posts

---
+++ client/src/views/PostsView.vue
@@ -0,0 +1,35 @@
+<template>
+    <main>
+      <the-navbar />
+      <the-posts-banner />
+      <the-posts :posts="posts"/>
+      <the-footer />
+    </main>
+</template>
+
+<script setup>
+import TheNavbar from '../components/TheNavbar.vue'
+import ThePostsBanner from '../components/ThePostsBanner.vue'
+import ThePosts from '../components/ThePosts.vue'
+import TheFooter from '../components/TheFooter.vue'
+</script>
+
+<script>
+import ApiService from '../services/ApiService';
+
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    this.fetchData('/posts/9');
+  },
+  methods: {
+    async fetchData(url) {
+      this.posts = await ApiService.fetchData(url);
+    }
+  }
+};
+</script>
